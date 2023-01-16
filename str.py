8ced9071ce7787f40fe73044bfcb4995ac243b27
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-16 19:44:20 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,56 @@
+# a=7
+# print("the value of a is",a)
+# print("the value of a is ",a)
+
+# b=""
+# print(b,type(b))
+
+# c=" "
+# print(c.isspace())
+# s=""
+# print(s.isspace())
+
+# d=5
+# print(type(d))
+# print(int(d))
+# print(float(d))
+# print(str(d))
+# print((d))
+
+# b=7.8
+# print(type(b))
+# print(int(b))
+# print(float(b))
+# print(str(b))
+# print((b))
+
+# a=input()
+# b=input()
+# print(a+b)
+
+# a="Python"
+# print("We are learnig %s"%a)
+# print('We are learning',a)
+# print('We are learning '+'' +a)
+
+# d='Dell'
+# print("One of the Laptop brand is",d)
+# print(""+d)
+# print(" "+d)
+# print("One of the Laptop brand is" +d)
+# print("One of the Laptop brand is" + " "+d)
+
+# d="12"
+# print(type(d))
+# print(int(d))
+# print(float(d))
+
+# print(int(5.6))
+# print(float(8))
+'''print(int("Python)) --> ValueError'''
+'''print(float("Python)) --> ValueEroor'''
+
+# e="45.89"
+# print(e,type(e))
+# g=(float(e))
+# print(int(g))
